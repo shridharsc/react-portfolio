f266b8f52fa655c13a6547a7985594c177516ea5
shridharsc <shridharschougule23@gmail.com>
2026-09-05 23:12:18 +0530

    test log

--- src/App.js
+++ src/App.js
@@ -99,13 +99,14 @@ export default function App() {
           About Me
         </h2>
 
-        <p className="text-gray-300 leading-8 text-sm md:text-lg max-w-4xl">
-          Results-driven Software Developer with 5+ years of experience
-          in full-stack development specializing in Angular, React,
-          Node.js, JavaScript, PostgreSQL, and cloud-based solutions.
+       <p className="text-gray-300 leading-8 text-sm md:text-lg max-w-5xl">
+          Results-driven Software Engineer with 5+ years of experience in
+          full-stack development specializing in Angular, React, Node.js,
+          TypeScript, JavaScript, PostgreSQL, REST APIs, AWS and Docker.
           Experienced in building scalable enterprise applications,
-          REST APIs, AI-powered chatbot integrations, and performance
-          optimization.
+          reusable Angular components, dynamic forms, authentication and
+          authorization systems. Also experienced in AI-powered solutions
+          using LLM, RAG, AI Agents and Flowise.
         </p>
       </section>
 
